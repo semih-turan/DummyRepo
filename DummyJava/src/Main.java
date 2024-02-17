8270ed5cd57aa236c236e8275c431612bb79be1c
Batuhan Yürüdür <batuhanyurudur@gmail.com>
2024-02-17 18:44:02 +0300

    ---

--- DummyJava/src/Main.java
+++ DummyJava/src/Main.java
@@ -3,6 +3,8 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.print("Hello and welcome!");
+
+
         System.out.println("Hallo und wilkommen!");
     }
 }
